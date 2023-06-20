e412ca684e2dc85600fbea3deadb8b63f9eb68a9
FelipeEnne <felipeenne@gmail.com>
2023-06-20 20:43:38 -0300

    finish style and start js

---
+++ script.js
@@ -0,0 +1,24 @@
+const api = {
+  key: "28fd15358cdecbc1a1dfef367e71acef",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+//Open weather app
+
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", getInput);
+
+function getInput(event) {
+  event.preventDefault();
+  if (event.type === "click") {
+    getData(search.value);
+  }
+}
+
+function getData(seach) {
+  fetch(`${api.base}weather?q=${search.value}&units=metric&appid=${api.key}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then(displayData);
+}
