fe08061b3fc1e72be90b651f2f39ec8b46cee890
FelipeEnne <felipeenne@gmail.com>
2023-06-22 20:12:30 -0300

    finish the project

--- script.js
+++ script.js
@@ -23,4 +23,66 @@ function getData(seach) {
     .then(displayData);
 }
 
-function displayData(response) {}
+function displayData(response) {
+  if (response.cod === "404") {
+    const error = document.querySelector(".error");
+    error.textContent = "Invalid city";
+    search.value = "";
+  } else {
+    const city = document.querySelector(".city");
+    city.textContent = `${response.name}, ${response.sys.country}`;
+
+    const today = new Date();
+    const date = document.querySelector(".date");
+    date.textContent = dataFunction(today);
+
+    const temp = document.querySelector(".temp");
+    temp.innerHTML = `Temp: ${Math.round(response.main.temp)} <span>°C</span>`;
+
+    const weather = document.querySelector(".weather");
+    weather.innerText = `Weather: ${response.weather[0].main}`;
+
+    const tempRange = document.querySelector(".temp-range");
+    tempRange.innerText = `Temp Range: ${Math.round(
+      response.main.temp_min
+    )}°C / ${Math.round(response.main.temp_max)}°C`;
+
+    const weatherIcon = document.querySelector(".weather-icon");
+    const iconURL = "http://openweathermap.org/img/w/";
+    weatherIcon.src = iconURL + response.weather[0].icon + ".png";
+
+    search.value = "";
+  }
+}
+function dataFunction(d) {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}, ${date} ${month} ${year}`;
+}
